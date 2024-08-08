505926987d40b21810c1cf1da180e42823754022
Mahimasingh456 <306aryansingh@gmail.com>
2024-08-08 11:28:55 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+const Footer = () => {
+  return (
+    <div>
+      <div className="logo font-bold text-white text-2xl">
+
+                
+<span className='bg-green-500'>&lt;</span>
+<span>Pass</span><span className='text-green-500'>SECURE/&gt;</span>
+
+</div>
+  <div>
+    Created with <img src="" alt=""/>
+  </div>
+    </div>
+  )
+}
+export default Footer
---
+++ Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className= 'bg-slate-800 text-white' >
+            <div className="mycontainer flex justify-between items-center px-4 py-5 h-14">
+            <div className="logo font-bold text-white text-2xl">
+
+                
+                <span className='bg-green-500'>&lt;</span>
+                <span>Pass</span><span className='text-green-500'>SECURE/&gt;</span>
+                
+                </div>
+            <ul>
+                <li className='flex gap-4 '>
+                    <a classname='hover:font-bold' href='/'>Home</a>
+                    <a classname='hover:font-bold' href='#'>About</a>
+                    <a classname='hover:font-bold' href="#">Contact</a>
+                </li>
+            </ul>
+            <button className='text-white bg-green-700 my-5 rounded-md flex gap-4 justify-between items-center ring-white ring-1'>
+                <img className='invert w-8 p-1 px-1' src="Octicons-mark-github.svg" alt="github logo"/>
+                <span className='font-bold '>Github</span>
+                 
+            </button>
+               
+
+            </div>
+            
+        
+        </nav>
+    )
+}
+export default Navbar
